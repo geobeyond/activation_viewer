f392d38be40bd54d3aaea8e950b168b432de6ca5
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-07-06 00:58:31 +0200

    Fix indentation

--- collection_viewer/urls.py
+++ collection_viewer/urls.py
@@ -14,18 +14,23 @@ api.register(CollectionFullResource())
 api.register(CollMapResource())
 
 
-urlpatterns = patterns('',
-    url(r'^/?$',
-       TemplateView.as_view(template_name='viewer_index.html'),
-       name='home'),
-    url(r'^composer/?',
-       TemplateView.as_view(template_name='map_composer.html'),
-       name='composer'),
-   # url(r'^maps/?',
-   #    TemplateView.as_view(template_name='maps.html'),
-   #    name='maps'),
+urlpatterns = patterns(
+    '',
+    # url(
+    #     r'^/?$',
+    #     TemplateView.as_view(template_name='site_base.html'), name='home'
+    # ),
+    url(r'^collections/viewer/?$',
+        TemplateView.as_view(template_name='viewer_index.html'),
+        name='viewer'),
+    url(r'^collections/composer/?',
+        TemplateView.as_view(template_name='map_composer.html'),
+        name='composer'),
+    # url(r'^maps/?',
+    #    TemplateView.as_view(template_name='maps.html'),
+    #    name='maps'),
     url(r'^collections/', include('collection_viewer.collection.urls')),
     url(r'', include(api.urls)),
- ) + urlpatterns
+) + urlpatterns
 
 handler403 = 'geonode.views.err403'

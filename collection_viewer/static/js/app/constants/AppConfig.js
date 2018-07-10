3d84bcb608238a39e73ddb71a07ba3e197979e5f
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-07-10 15:13:36 +0200

    Fix composer url for new routing

--- collection_viewer/static/js/app/constants/AppConfig.js
+++ collection_viewer/static/js/app/constants/AppConfig.js
@@ -1,5 +1,5 @@
 export default {
-  COMPOSER_URL: '/composer',
+  COMPOSER_URL: '/collections/composer',
   LIST_ACTIVATIONS_URL: '/api/collections/',
   FULL_ACTIVATIONS_URL: '/api/collections-full/'
 };

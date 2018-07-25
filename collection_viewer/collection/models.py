a77038df12d380fff9b9457e00b95544c22f0147
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-07-25 16:07:19 +0200

    Add generation of thumbnail for collections

--- collection_viewer/collection/models.py
+++ collection_viewer/collection/models.py
@@ -1,5 +1,10 @@
 import datetime
-
+import os
+import logging
+from PIL import Image
+import requests
+from urlparse import urljoin
+from collections import OrderedDict
 from django.db import models
 from django.db.models import signals
 from django.contrib.auth.models import Group, Permission
@@ -10,9 +15,14 @@ from guardian.shortcuts import assign_perm, get_groups_with_perms, get_users_wit
 from collection_viewer import settings
 from geonode.layers.models import Layer
 from geonode.base.models import Region
+from geonode.geoserver.helpers import ogc_server_settings
+from django.core.files.storage import default_storage as storage
+from django.core.files.base import ContentFile
+
 
 LAYERTYPES = settings.LAYERTYPES
 
+logger = logging.getLogger(__name__)
 
 
 class CollectionType(models.Model):
@@ -182,6 +192,67 @@ class Collection(models.Model):
             collection_id=self.collection_id
         ).update(bbox_x0=x0, bbox_x1=x1, bbox_y0=y0, bbox_y1=y1)
 
+    def set_thumbnail_from_mapsets(self):
+        thumbnail_name = "collection-{0}-thumb.png".format(
+            self.collection_id
+        )
+        upload_to = 'thumbs/'
+        upload_path = os.path.join(upload_to, thumbnail_name)
+        thumb = None
+        coll_layers = OrderedDict()
+        ms_layers = [
+            MapSet.objects.filter(
+                id=mapset.id
+            )[0].layers.all() for mapset in self.mapset_set.all()
+        ]
+        for ms_layer in ms_layers:
+            for i in range(ms_layer.count()):
+                coll_layers.update(
+                    {
+                        "{0}".format(
+                            os.path.basename(ms_layer[i].layer.detail_url)
+                        ): "{0}".format(
+                            ms_layer[i].layer.thumbnail_url
+                        )
+                    }
+                )
+        local_layers = coll_layers.keys()
+        layers = ",".join(local_layers).encode('utf-8')
+        params = {
+            'layers': layers,
+            'format': 'image/png8',
+            'width': 200,
+            'height': 150,
+            'TIME': '-99999999999-01-01T00:00:00.0Z/99999999999-01-01T00:00:00.0Z'
+        }
+        p = "&".join("%s=%s" % item for item in params.items())
+        thumbnail_create_url = ogc_server_settings.LOCATION + \
+            "wms/reflect?" + p
+        try:
+            thumb = requests.get(thumbnail_create_url, stream=True)
+            thumb.raw.decode_content = True
+            thumb_result = Image.open(thumb.raw)
+            thumb_result.save(thumbnail_name)
+
+            if storage.exists(upload_path):
+                # Delete if exists otherwise the (FileSystemStorage)
+                # implementation will create a new file with a unique name
+                storage.delete(os.path.join(upload_path))
+
+            storage.save(upload_path, ContentFile(thumb_result))
+        except Exception:
+            logger.error(
+                'Error when generating the thumbnail for resource %s.' %
+                self.collection_id)
+            logger.error('Check permissions for file %s.' % upload_path)
+
+        Collection.objects.filter(
+            collection_id=self.collection_id
+        ).update(thumbnail_url=urljoin(
+            settings.SITEURL, os.path.join('uploaded', upload_path)
+        ))
+
+
 class CollectionMaps(models.Model):
     """
         Store information about saved maps such as layers
@@ -211,6 +282,7 @@ class ExternalLayer(models.Model):
 def mapset_layers_changed(instance, *args, **kwargs):
     instance.set_bbox_from_layers()
     instance.collection.set_bbox_from_mapsets()
+    instance.collection.set_thumbnail_from_mapsets()
 
 
 signals.m2m_changed.connect(
